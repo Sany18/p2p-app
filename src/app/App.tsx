0877887340e9225d07c68ab7cf5b0c8dde2a68ff
Alexandr Tantsiura <o.tantsiura@spd-ukraine.com>
2024-06-09 16:20:03 +0300

    Add some basics

---
+++ src/app/App.tsx
@@ -0,0 +1,16 @@
+// import logo from 'assets/logo.svg';
+// {/* <img src={logo} className="App-logo" alt="logo" /> */}
+
+import React from 'react';
+
+import './App.scss';
+
+function App() {
+  return (
+    <div className='p-2'>
+      test
+    </div>
+  );
+}
+
+export default App;
